8ac885f27a3b3d7eec1d0b09ce3ca1f6e8f1e712
aviral5rivastava <aviral.lonewolf@gmail.com>
2022-11-27 15:44:39 +0530

    added database connection

--- server/server.js
+++ server/server.js
@@ -4,3 +4,22 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
+
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(cors());
+
+
+const DB_CONNECTION = process.env.DATABASE_URL;
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running @ : http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.error(error));
+
